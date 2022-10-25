9cc2ca6114b72e3182143fa38d7cdeda6328d232
async-devil <async.devil@gmail.com>
2022-10-25 17:33:35 +0300

    feat: update index script

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
-import fastify from "fastify";
+import { join } from "path";
 
-import SandwichesController from "./sandwiches/sandwiches.controller";
+import autoLoad from "@fastify/autoload";
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import fastify, { RouteOptions } from "fastify";
 
 const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
 
-void server.register(SandwichesController);
+const routes: RouteOptions[] = [];
+
+void server.register(autoLoad, {
+	dir: join(__dirname, "routes"),
+	dirNameRoutePrefix: false,
+	options: { prefix: "/api" },
+});
+
+server.addHook("onRoute", (route) => {
+	routes.push(route);
+});
+
+function printRoute(route: RouteOptions) {
+	console.log(`Route ${route.method.toString()} ${route.url} initialized`);
+}
 
 server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
 	if (err) {
 		console.error(err);
 		process.exit(1);
 	}
-	console.log(`Server is listening at ${address}`);
+	console.log(`Server is listening at ${address}\n`);
+
+	for (const route of routes) printRoute(route);
 });
