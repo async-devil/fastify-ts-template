697c57978a56193d3657a80ffa393db14ebecf87
async-devil <async.devil@gmail.com>
2022-10-25 17:33:50 +0300

    feat: update sandwich route

--- src/routes/sandwiches/dto/create-sandwich.dto.ts
+++ src/routes/sandwiches/dto/create-sandwich.dto.ts
@@ -4,9 +4,3 @@ export const CreateSandwichBodyDto = Type.Object({
 	name: Type.String(),
 	ingredients: Type.Record(Type.String(), Type.Number()),
 });
-
-export const CreateSandwichResponseDto = Type.Object({
-	id: Type.Number(),
-	name: Type.String(),
-	ingredients: Type.Record(Type.String(), Type.Number()),
-});
